73b6ab55bd78250c6f6b3e0c44b0254dfbdec348
David <dawid.prokopek@gmail.com>
2019-05-23 11:21:08 +0200

    styling

--- src/component/toolbar/Toolbar.js
+++ src/component/toolbar/Toolbar.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+import classes from './Toolbar.css'
 
 const toolbar = (props) => {
 
     return (
-        <div className='Toolbar'>
+        <div className={classes.Toolbar}>
                 <div>Foodie</div>
                 <IconButton color="primary">
                     <FavoriteBorder/>
