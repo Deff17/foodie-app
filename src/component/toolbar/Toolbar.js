0aa624c76d3d8620d0ca6debed1bf4bc157d2d34
michaelloo35 <michaelloo35@gmail.com>
2019-06-09 00:53:10 +0200

    chups + autocompletion

--- src/component/toolbar/Toolbar.js
+++ src/component/toolbar/Toolbar.js
@@ -2,25 +2,33 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
-import Autocomplete from '../autocomplete/Autocomplete'
 import image from './logo.png'
+import {ReactAutosuggestExample} from './examples/react-autosuggest.js'
 
 const toolbar = (props) => {
     return (
         <div className={props.classes.ToolbarMain}>
-            <div><a href='/'> <img className='profile-image' alt='icon' src={image} width="150"/> </a></div>
+            <div><a href='/'>
+                <Button variant="contained"><a href='/login'>Log In </a> </Button>
+                <Button variant="contained"><a href='/register'>Register</a> </Button>
+                <IconButton>
+                    <a><FavoriteBorder/></a>
 
-            <IconButton>
-                <a><FavoriteBorder/></a>
+                </IconButton>
 
-            </IconButton>
-            <Button variant="contained"><a href='/login'>Log In </a> </Button>
-            <Button variant="contained"><a href='/register'>Register</a> </Button>
-            <Autocomplete classes={props.classes}/>
+                <img className='profile-image' alt='icon' src={image} width="150"/> </a>
+            </div>
+
+
+                <ReactAutosuggestExample
+                    label='Food search'
+                    placeholder='Select ingredients'
+                />
         </div>
     );
 
 
 }
+
 
 export default toolbar; 
