e75c24d00b96c41a3af16553b7e0fab1dafc339d
michaelloo35 <michaelloo35@gmail.com>
2019-06-08 23:57:23 +0200

    Logo

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import React, { Component } from 'react';
-import Layout from './component/Layout'
-import classes from './App.css'
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import MainPage from './container/mainPage/MainPage';
 import Results from './container/results/Results';
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="container">
-          <Route exact path="/" component={MainPage} />
-          <Route exact path="/results" component={Results} />
-          <Route exact path="/login" component={MainPage} />
-          <Route exact path="/register" component={Results} />
-        </div>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="container">
+                    <Route exact path="/" component={MainPage}/>
+                    <Route exact path="/results" component={Results}/>
+                    <Route exact path="/login" component={MainPage}/>
+                    <Route exact path="/register" component={Results}/>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
--- src/component/toolbar/Toolbar.js
+++ src/component/toolbar/Toolbar.js
@@ -3,19 +3,20 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
 import Autocomplete from '../autocomplete/Autocomplete'
+import image from './logo.png'
 
 const toolbar = (props) => {
-
     return (
         <div className={props.classes.ToolbarMain}>
-            <div><a href='/'> FOODIE </a></div>
+            <div><a href='/'> <img className='profile-image' alt='icon' src={image} width="150"/> </a></div>
+
             <IconButton>
-                <a><FavoriteBorder /></a>
+                <a><FavoriteBorder/></a>
 
             </IconButton>
-            <Button variant="contained" ><a  href='/login'>Log In </a> </Button>
-            <Button variant="contained" ><a  href='/register'>Register</a> </Button>
-            <Autocomplete classes={props.classes} />
+            <Button variant="contained"><a href='/login'>Log In </a> </Button>
+            <Button variant="contained"><a href='/register'>Register</a> </Button>
+            <Autocomplete classes={props.classes}/>
         </div>
     );
 
