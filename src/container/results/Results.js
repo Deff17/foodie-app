76da1e7d8a0a39ec07401b787a3c72258fbcf8f8
David <dawid.prokopek@gmail.com>
2019-06-11 21:08:51 +0200

    add recipes to results

--- src/container/results/Results.js
+++ src/container/results/Results.js
@@ -4,23 +4,61 @@ import Result from '../../component/result/Result';
 import Route from 'react-router-dom';
 import classes from './Results.css';
 
+const recipes = [{
+    ingridients: ['Salmon', 'Pepper', 'Egg'],
+    title: 'Salmon with papper',
+    recepie: ' DOOOOOOOOOOOOOOOOOOOO IT, JUST DOOOOOOOOOOOO IT, IT\'S just SALMON'
+}, {
+    ingridients: ['Apple', 'Lemon', 'Egg', 'Flour'],
+    title: 'APPLE PIE',
+    recepie: ' DOOOOOOOOOOOOOOOOOOOO IT, JUST DOOOOOOOOOOOO IT, IT\'S just APPLE PIE'
+},
+{
+    ingridients: ['Chicken', 'Lemon', 'Tomato'],
+    title: 'Chicken in tomato souce',
+    recipe: ' DOOOOOOOOOOOOOOOOOOOO IT, JUST DOOOOOOOOOOOO IT, IT\'S just Chicken in tomato souce'
+}
+]
 
 class Results extends Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            selectedItems: JSON.parse(localStorage.getItem('SelectedItems')),
+        }
+    }
+
+    getRecipes = (recipes) => {
+        const addedItems = this.state.selectedItems.filter(item => item.type === 'add');
+        const removedItems = this.state.selectedItems.filter(item => item.type === 'remove');
+        console.log(removedItems)
+        // const properRecipes = recipes.filter(recipe => { recipe.some(r=> addedItems.indexOf(r) >= 0) && !recipe.some(r=> removedItems.indexOf(r) >= 0)})
+        // console.log(properRecipes)
+        return [];
+
     }
 
     render() {
+        console.log('RESULT')
+        console.log(this.state.selectedItems)
         return (
             <div >
-                <Toolbar isMainPage={false}/>
-                <Result key={1}/>
+                <Toolbar isMainPage={false} />
+                {
+                    this.getRecipes(recipes).map(recipe => {
+                        return (<Result key={recipe.title} title={recipe.title} recipe={recipe.recipe} ingridients={recipe.ingridients}/>)
+                    })
+                }
+
             </div>
         )
     }
 
 }
+
+
 
 
 export default Results;
